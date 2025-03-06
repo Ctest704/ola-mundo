8d16b6653bc22d61b2df95d8f6d0590be96c7bd0
Ctest704 <cgithub9@gmail.com>
2025-03-06 13:03:02 -0300

    atualização de foto

--- src/Componentes/Banner/Banner.js
+++ src/Componentes/Banner/Banner.js
@@ -25,6 +25,7 @@ export default function Banner() {
                 <img 
                     className={styles.circuloColorido}
                     src={circuloColorido}
+                    alt='circulo colorido'
                     aria-hidden={true}
                 />
 
